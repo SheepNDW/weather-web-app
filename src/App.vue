f94444e76c090db8207621c4405c29879f33bd27
SheepNDW <hank3681@gmail.com>
2021-10-16 17:53:53 +0800

    新增載入和錯誤提示頁面

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,52 @@
 <template>
-  <div class="container">
-    <!-- weather(left) -->
-    <WeatherSide :cityInfo="cityInfo" />
+  <div id="app">
+    <div class="container" v-show="!cityInfo.isLoading">
+      <!-- weather(left) -->
+      <WeatherSide :cityInfo="cityInfo" />
 
-    <!-- info(right) -->
-    <div class="info-side">
-      <div class="today-info-container">
-        <div class="today-info">
-          <!-- MaxT -->
-          <div class="MaxT clear">
-            <span class="title">最高溫</span>
-            <span class="value"
-              >{{ Math.round(cityInfo.weatherDatas[0].max_temp) }}°C</span
-            >
-          </div>
-          <!-- MinT -->
-          <div class="MinT clear">
-            <span class="title">最低溫</span>
-            <span class="value"
-              >{{ Math.round(cityInfo.weatherDatas[0].min_temp) }}°C</span
-            >
-          </div>
-          <!-- humidity -->
-          <div class="humidity clear">
-            <span class="title">濕度</span>
-            <span class="value">{{ cityInfo.weatherDatas[0].humidity }}%</span>
+      <!-- info(right) -->
+      <div class="info-side">
+        <div class="today-info-container">
+          <div class="today-info">
+            <!-- MaxT -->
+            <div class="MaxT clear">
+              <span class="title">最高溫</span>
+              <span class="value"
+                >{{ Math.round(cityInfo.weatherDatas[0].max_temp) }}°C</span
+              >
+            </div>
+            <!-- MinT -->
+            <div class="MinT clear">
+              <span class="title">最低溫</span>
+              <span class="value"
+                >{{ Math.round(cityInfo.weatherDatas[0].min_temp) }}°C</span
+              >
+            </div>
+            <!-- humidity -->
+            <div class="humidity clear">
+              <span class="title">濕度</span>
+              <span class="value"
+                >{{ cityInfo.weatherDatas[0].humidity }}%</span
+              >
+            </div>
           </div>
         </div>
+
+        <WeekList :cityInfo="cityInfo" />
+
+        <Search />
       </div>
-
-      <WeekList :cityInfo="cityInfo" />
-
-      <Search />
+    </div>
+    <!-- 載入中&錯誤提示 -->
+    <div class="loadingPage" v-show="cityInfo.isLoading">
+      <div class="monster">
+        <div class="eye">
+          <div class="eyeBall"></div>
+        </div>
+        <div class="mouth"></div>
+      </div>
+      <h2 v-show="!cityInfo.errMsg">Loading...</h2>
+      <h2 v-show="cityInfo.errMsg">{{ cityInfo.errMsg }}</h2>
     </div>
   </div>
 </template>
@@ -52,6 +67,8 @@ export default {
   data() {
     return {
       cityInfo: {
+        isLoading: false,
+        errMsg: "",
         city: "Taipei",
         weatherDatas: [],
       },
@@ -113,6 +130,115 @@ body {
   color: #fff;
 }
 
+.loadingPage {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 750px;
+  height: 435px;
+  background-color: $colorContainer;
+  border-radius: 25px;
+  color: #fff;
+
+  .monster {
+    width: 110px;
+    height: 110px;
+    background-color: #e55a54;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: column;
+    cursor: pointer;
+    margin: 10px;
+    position: relative;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
+    transition: 0.5s;
+    animation: jumping 0.8s infinite alternate;
+
+    .eye {
+      width: 40%;
+      height: 40%;
+      border-radius: 50%;
+      background-color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .eyeBall {
+      width: 50%;
+      height: 50%;
+      border-radius: 50%;
+      background-color: #0c4475;
+    }
+
+    .mouth {
+      width: 32%;
+      height: 12px;
+      background-color: white;
+      border-radius: 12px;
+      margin-top: 15%;
+    }
+
+    &:before,
+    &:after {
+      content: "";
+      display: block;
+      width: 20%;
+      height: 10px;
+      background-color: white;
+      position: absolute;
+      left: 50%;
+      top: -10px;
+      border-radius: 10px;
+    }
+
+    &:before {
+      transform: translateX(-70%) rotate(45deg);
+    }
+
+    &:after {
+      transform: translateX(-30%) rotate(-45deg);
+    }
+
+    &.blue {
+      background-color: #0c4475;
+      animation-delay: 0.5s;
+
+      .mouth,
+      .eyeBall {
+        background-color: #e55a54;
+      }
+    }
+  }
+  @keyframes jumping {
+    50% {
+      top: 0;
+      box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
+    }
+    100% {
+      top: -50px;
+      box-shadow: 0px 120px 50px rgba(0, 0, 0, 0.2);
+    }
+  }
+  @keyframes eyeMove {
+    0%,
+    10% {
+      transform: translate(50%);
+    }
+    90%,
+    100% {
+      transform: translate(-50%);
+    }
+  }
+  .monster .eyeBall {
+    animation: eyeMove 1.6s infinite alternate;
+  }
+}
+
 //--- right part start ---
 .info-side {
   width: 450px;
@@ -138,11 +264,6 @@ body {
   .value {
     float: right;
   }
-}
-
-.active {
-  background-color: #fff;
-  color: #222831;
 }
 
 //--- right part end ---
